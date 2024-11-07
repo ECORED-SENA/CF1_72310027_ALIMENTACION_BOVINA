0af9b5dca781f6e363821070382ce2821aa5bdd7
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2024-11-06 20:48:46 -0500

    PDF || Descargable

--- src/config/global.js
+++ src/config/global.js
@@ -96,7 +96,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/72310027_CF01_DU.pdf',
       },
       {
         icono: 'fas fa-download',
@@ -265,6 +265,21 @@ export default {
         {
           nombre: 'Luis Jesús Pérez Madariaga',
           cargo: 'Desarrollador <i>full stack</i>',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+        },
+        {
+          nombre: 'Alejandro Delgado Acosta',
+          cargo: 'Intérprete lenguaje de señas',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+        },
+        {
+          nombre: 'Daniela Muñoz Bedoya',
+          cargo: 'Animador y productor multimedia',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+        },
+        {
+          nombre: 'María Carolina Tamayo López',
+          cargo: 'Locución',
           centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
       ],
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -157,7 +157,7 @@
 
     figure.mt-4(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/gqjMJqunp80?si=eeLfpU-evxSvxGbz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
     .cont_decorativo.mb-4.mt-5(data-aos="fade-right")
       .row
